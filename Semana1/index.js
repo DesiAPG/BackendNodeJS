410aecf91bd2471b0c031950ba13d637021c966d
DesiAPG <desireapgames@gmail.com>
2022-04-21 09:03:47 -0500

    API REST

--- Semana1/index.js
+++ Semana1/index.js
@@ -1,6 +1,36 @@
 const express = require("express");
+const { port } = require("./config");
 const app = express();
 
-app.get('/',(req,res)=>{
-    
-})
+const users = [];
+
+// Metodo get para obtener la ruta principal
+app.get("/", (req, res) => {
+  res.json(users);
+  return res.json(users);
+});
+
+app.post("/", (req, res) => {
+  const user = req.body;
+  users.push(users);
+  return res.json(users);
+});
+
+app.put("/", (req, res) => {
+  users.map((users) => {
+      if(id === user.id){
+          
+      }
+  });
+  return res.json(users);
+});
+
+app.delete("/:id", (req, res) => {
+  const users = users.filter((user) => id !== user.id);
+  return res.json(users);
+});
+
+// Escuchar en el puerto 4000
+app.listen(port, () => {
+  console.log("Listening on port " + port);
+});
