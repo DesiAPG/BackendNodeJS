f81eedc609ce5e1383ac27b8f75e5a3cba73b92d
DesiAPG <desireapgames@gmail.com>
2022-04-21 08:14:35 -0500

    added express

--- Semana1/index.js
+++ Semana1/index.js
@@ -1,19 +1,6 @@
-const { env, port } = require("./config");
+const express = require("express");
+const app = express();
 
-const http = require("http");
-const server = http.createServer();
-
-server.on("request", (request, response) => {
-  if (request.method === "POST" && request.url == "/datos") {
-    // TODO: Regresar al momento de ver streams
-  }
-
-  response.statusCode = "200";
-
-  response.end("Hola mundo");
-});
-
-server.listen(port);
-console.log("Servidor funcionando en: http://localhost:" + port);
-
-console.log(env);
+app.get('/',(req,res)=>{
+    
+})
