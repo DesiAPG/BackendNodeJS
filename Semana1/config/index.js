47bef46ca51256a8f086de156b21465b23ec8c3e
DesiAPG <desireapgames@gmail.com>
2022-04-27 08:49:35 -0500

    mongoose

--- Semana1/config/index.js
+++ Semana1/config/index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 
 const config = {
-  env: process.env.NODE_ENV,
   port: process.env.PORT,
+  env: process.env.NODE_ENV,
+  dbUsername: process.env.DB_USERNAME,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
 };
 
 module.exports = config;
