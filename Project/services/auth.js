e72b31614ddb66216afe4929666e1c9f9b57b068
DesiAPG <desireapgames@gmail.com>
2022-05-05 08:52:02 -0500

    auth

--- Project/services/auth.js
+++ Project/services/auth.js
@@ -1,24 +1,43 @@
 const jwt = require("jsonwebtoken");
 const { jwtSecret } = require("../config");
 const User = require("./users");
+const bcrypt = require("bcrypt");
 
 class Auth {
   login(data) {
     return this.#createToken(data);
   }
   async signup(data) {
+    if (data.password) {
+      data.password = await this.#encrypt(data.password);
+    }
     userServ = new User();
     const user = await userServ.create(data);
-    const token = this.#createToken();
+    const userData = {
+      name: user.name,
+      email: user.email,
+      id: user.id,
+    };
+    const token = this.#createToken(userDdata);
     return {
-      user,
+      user: userData,
       token,
     };
   }
 
-  #createToken(payload) {
-    const token = jwt.sign(payload, jwtSecret);
+  #createToken(payLoad) {
+    const token = jwt.sign(payLoad, jwtSecret);
     return token;
+  }
+
+  async #encrypt(string) {
+    try {
+      const salt = await bcrypt.genSalt();
+      const hash = await bcrypt.hash(string, salt);
+      return hash;
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 module.exports = Auth;
